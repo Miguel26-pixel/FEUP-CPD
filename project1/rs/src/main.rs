d923e9ddd3046b76908e1c3324db6c0da1542566
David Preda <dtpreda30@gmail.com>
2022-03-18 18:53:38 +0000

    Refactor rust version

--- project1/rs/src/main.rs
+++ project1/rs/src/main.rs
@@ -1,82 +1,13 @@
 use papi_sys::*;
-use std::{io,cmp,time::Instant};
+use std::{time::Instant};
+use std::os::raw::c_longlong;
 
 const PAPI_L1_DCM: i32 = -2147483648;
 const PAPI_L2_DCM: i32 = -2147483646;
 
-fn matrix_multiplication(matrix_size: i32) -> () {
-    let mut dot_product:f64;
-
-    let first_factor: Vec<f64> = vec![1.0;(matrix_size*matrix_size) as usize];
-    let mut second_factor: Vec<f64> = Vec::with_capacity((matrix_size*matrix_size) as usize);
-    let mut result_matrix: Vec<f64> = Vec::with_capacity((matrix_size*matrix_size) as usize);
-
-    for i in 0..matrix_size {
-        for _j in 0..matrix_size {
-            second_factor.push((i+1) as f64);
-        }
-    }
-
-    let now = Instant::now();
-
-    for i in 0..matrix_size {
-        for j in 0..matrix_size {
-            dot_product = 0.0;
-            for k in 0..matrix_size {
-                dot_product += first_factor[(i*matrix_size+k) as usize] * second_factor[(k*matrix_size+j) as usize];
-            }
-            result_matrix.push(dot_product);
-        }
-    }
-
-    let elapsed_time = now.elapsed();
-
-    println!("Time: {} miliseconds", elapsed_time.as_millis());
-
-    for j in 0..cmp::min(10,matrix_size) {
-        print!("{} ",result_matrix[j as usize]);
-    }
-
-    println!();
-}
-
-fn matrix_line_multiplication(matrix_size: i32) -> () {
-    let first_factor: Vec<f64> = vec![1.0;(matrix_size*matrix_size) as usize];
-    let mut second_factor: Vec<f64> = Vec::with_capacity((matrix_size*matrix_size) as usize);
-    let mut result_matrix: Vec<f64> = vec![0.0;(matrix_size*matrix_size) as usize];
-
-    for i in 0..matrix_size {
-        for _j in 0..matrix_size {
-            second_factor.push((i+1) as f64);
-        }
-    }
-
-    let now = Instant::now();
-
-    for i in 0..matrix_size {
-        for j in 0..matrix_size {
-            for k in 0..matrix_size {
-                result_matrix[(i * matrix_size + k) as usize] += first_factor[(i * matrix_size + j) as usize] * second_factor[(j * matrix_size + k) as usize];
-            }
-        }
-    }
-
-    let elapsed_time = now.elapsed();
-
-    println!("Time: {} miliseconds", elapsed_time.as_millis());
-
-    for j in 0..cmp::min(10,matrix_size) {
-        print!("{} ",result_matrix[j as usize]);
-    }
-
-    println!();
-}
-
-fn main() {
+fn papi_init() -> i32 {
     let mut ret:i32;
     let mut event_set:i32 = PAPI_NULL;
-    let mut cache_miss_count:[i64;2] = [0,0];
-    let values: *mut i64 = &mut cache_miss_count as *mut i64;
 
     unsafe {
         ret = PAPI_library_init(PAPI_VER_CURRENT);
@@ -92,58 +23,13 @@ fn main() {
         assert_eq!(ret as u32, PAPI_OK);
     }
 
-    loop {
-        println!("1. Multiplication");
-		println!("2. Line Multiplication");
-		println!("3. Block Multiplication"); 
-        println!("0. Exit"); 
-		println!("Selection?: ");
+    return event_set;
+}
 
-        let mut option = String::new();
-        let stdin = io::stdin(); // We get `Stdin` here.
-        stdin.read_line(&mut option).unwrap();
-
-        if option == "0\n" {
-            break;
-        }
-
-        println!("Dimensions: lins=cols ? ");
-
-        let mut buffer = String::new();
-        let stdin = io::stdin(); // We get `Stdin` here.
-        stdin.read_line(&mut buffer).unwrap();
-
-        let matrix_size: i32;
-        let trimmed = buffer.trim();
-        
-        match trimmed.parse::<i32>() {
-            Ok(i) => matrix_size = i,
-            Err(..) => continue,
-        };
-
-        unsafe {
-            ret = PAPI_start(event_set);
-            assert_eq!(ret as u32, PAPI_OK);
-        }
-
-        if option == "1\n" {
-            matrix_multiplication(matrix_size);
-        }
-        if option == "2\n" {
-            matrix_line_multiplication(matrix_size);
-        }
-
-        unsafe {
-            ret = PAPI_stop(event_set, values);
-            assert_eq!(ret as u32, PAPI_OK);
-        }
-
-        println!("L1 Cache Misses: {}",cache_miss_count[0]);
-        println!("L2 Cache Misses: {}",cache_miss_count[1]);
-    }
-
+fn papi_destroy(mut event_set: i32) -> () {
+    let mut ret: i32;
     unsafe {
-        ret = PAPI_remove_event( event_set, PAPI_L1_DCM );
+        ret = PAPI_remove_event(event_set, PAPI_L1_DCM );
         assert_eq!(ret as u32, PAPI_OK);
 
         ret = PAPI_remove_event( event_set, PAPI_L2_DCM );
@@ -152,4 +38,115 @@ fn main() {
         ret = PAPI_destroy_eventset( &mut event_set );
         assert_eq!(ret as u32, PAPI_OK);
     }
+}
+
+fn dot_product(matrix_size: i32, event_set: i32) -> [u128; 3] {
+    let mut dot_product: f64;
+    let mut cache_miss_count: [c_longlong; 2] = [0, 0];
+    let cache_miss_count_ptr: *mut c_longlong = &mut cache_miss_count as *mut c_longlong;
+
+    let first_factor: Vec<f64> = vec![1.0;(matrix_size*matrix_size) as usize];
+    let mut second_factor: Vec<f64> = Vec::with_capacity((matrix_size*matrix_size) as usize);
+    let mut result_matrix: Vec<f64> = Vec::with_capacity((matrix_size*matrix_size) as usize);
+
+    for i in 0..matrix_size {
+        for _j in 0..matrix_size {
+            second_factor.push((i+1) as f64);
+        }
+    }
+
+    unsafe {
+        if PAPI_start(event_set) as u32 != PAPI_OK {
+            panic!("Unable to start PAPI");
+        }
+    }
+
+    let starting_time = Instant::now();
+
+    for i in 0..matrix_size {
+        for j in 0..matrix_size {
+            dot_product = 0.0;
+            for k in 0..matrix_size {
+                dot_product += first_factor[(i*matrix_size+k) as usize] * second_factor[(k*matrix_size+j) as usize];
+            }
+            result_matrix.push(dot_product);
+        }
+    }
+
+    unsafe {
+        if PAPI_stop(event_set, cache_miss_count_ptr) as u32 != PAPI_OK {
+            panic!("Unable to stop PAPI");
+        }
+    }
+
+    let elapsed_time = starting_time.elapsed();
+
+    [elapsed_time.as_millis(), cache_miss_count[0] as u128, cache_miss_count[1] as u128]
+}
+
+fn line_multiplication(matrix_size: i32, event_set: i32) -> [u128; 3] {
+    let mut cache_miss_count: [c_longlong; 2] = [0, 0];
+    let cache_miss_count_ptr: *mut c_longlong = &mut cache_miss_count as *mut c_longlong;
+
+    let first_factor: Vec<f64> = vec![1.0;(matrix_size*matrix_size) as usize];
+    let mut second_factor: Vec<f64> = Vec::with_capacity((matrix_size*matrix_size) as usize);
+    let mut result_matrix: Vec<f64> = vec![0.0;(matrix_size*matrix_size) as usize];
+
+    for i in 0..matrix_size {
+        for _j in 0..matrix_size {
+            second_factor.push((i+1) as f64);
+        }
+    }
+
+    unsafe {
+        if PAPI_start(event_set) as u32 != PAPI_OK {
+            panic!("Unable to start PAPI");
+        }
+    }
+
+    let starting_time = Instant::now();
+
+    for i in 0..matrix_size {
+        for j in 0..matrix_size {
+            for k in 0..matrix_size {
+                result_matrix[(i * matrix_size + k) as usize] += first_factor[(i * matrix_size + j) as usize] * second_factor[(j * matrix_size + k) as usize];
+            }
+        }
+    }
+
+    unsafe {
+        if PAPI_stop(event_set, cache_miss_count_ptr) as u32 != PAPI_OK {
+            panic!("Unable to stop PAPI");
+        }
+    }
+
+    let elapsed_time = starting_time.elapsed();
+
+    [elapsed_time.as_millis(), cache_miss_count[0] as u128, cache_miss_count[1] as u128]
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let algorithm = &args[1];
+
+    let event_set = papi_init();
+
+    let results = match algorithm.as_str() {
+        "dot" => dot_product(1000, event_set),
+        "line" => line_multiplication(1000, event_set),
+        "block" => [0, 0, 0],
+        _ => {
+            println!("Invalid argument. Correct usage: ./matrixproduct <dot | line | block>");
+            [0, 0, 0]
+        }
+    };
+
+    papi_destroy(event_set);
+    
+    if results == [0, 0, 0] {
+        return ();
+    }
+
+    println!("{:?}", results);
 }
