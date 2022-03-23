cc19ac8a7a2c3500f69183dcbb9b2858bfe42d74
David Preda <dtpreda30@gmail.com>
2022-03-23 18:21:18 +0000

    Finish template chart script

--- project1/charts/charts.py
+++ project1/charts/charts.py
@@ -16,11 +16,28 @@ cpp_metrics = [[int(x[0]), float(x[1]), float(x[2]), float(x[3]), float(x[4])] f
 rs_metrics = [[int(x[0]), float(x[1]), float(x[2]), float(x[3])] for x in rs_metrics]
 
 cpp_xarr = np.array([x[0] for x in cpp_metrics])
-cpp_yarr = np.array([x[1]*1000 for x in cpp_metrics])
+cpp_yarr = np.array([x[1] for x in cpp_metrics])
 
 rs_xarr = np.array([x[0] for x in rs_metrics])
-rs_yarr = np.array([x[1] for x in rs_metrics])
+rs_yarr = np.array([x[1]/1000 for x in rs_metrics])
 
-plt.plot(cpp_xarr, cpp_yarr, "r")
-plt.plot(rs_xarr, rs_yarr, "g")
+
+for index in range(len(cpp_xarr)):
+    plt.plot([cpp_xarr[index], cpp_xarr[index]], [rs_yarr[index], cpp_yarr[index]], color="k")
+plt.plot(rs_xarr, rs_yarr, "g", marker="D", label="Rust")
+plt.plot(cpp_xarr, cpp_yarr, "r", marker="D", label="C++")
+
+plt.legend(loc="upper left", frameon=False)
+plt.xlim(xmin=400, xmax=3200)
+plt.grid()
+
+for x_item, y_item in np.nditer([rs_xarr, rs_yarr]):
+    label = "{:.2f}".format(y_item) + "s"
+    plt.annotate(label, (x_item, y_item), textcoords="offset points", xytext=(-25,10),ha="center")
+for x_item, y_item in np.nditer([cpp_xarr, cpp_yarr]):
+    label = "{:.2f}".format(y_item) + "s"
+    plt.annotate(label, (x_item, y_item), textcoords="offset points", xytext=(20,-15),ha="center")
+
+plt.xlabel("Matrix Size (n in a nxn matrix)")
+plt.ylabel("Time of execution (seconds)")
 plt.show()
