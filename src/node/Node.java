7f580752584464973798ce90d33b9ace94958a19
David Preda <dtpreda30@gmail.com>
2022-05-11 12:17:01 +0100

    Start Node development

--- src/node/Node.java
+++ src/node/Node.java
@@ -1,4 +1,37 @@
 package node;
 
+import node.membership.log.Log;
+
 public class Node {
+    private int membership_counter;
+    private Log log;
+
+    public Node() {
+        this.membership_counter = 0;
+        this.log = new Log();
+    }
+
+    public boolean join() {
+        if (!this.canJoin()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean leave() {
+        if (!this.canLeave()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean canJoin() {
+        return this.membership_counter % 2 == 0;
+    }
+
+    private boolean canLeave() {
+        return this.membership_counter % 2 != 0;
+    }
 }
