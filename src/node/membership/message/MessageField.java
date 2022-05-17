3b21116eb56c11d7d3a4de85d57e72fb8d97d8d3
David Preda <dtpreda30@gmail.com>
2022-05-17 18:36:04 +0100

    Add MessageField and derived class

---
+++ src/node/membership/message/MessageField.java
@@ -0,0 +1,23 @@
+package node.membership.message;
+
+import java.util.List;
+
+public abstract class MessageField {
+    public FieldType fieldType;
+
+    protected MessageField(FieldType fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    protected char translateFieldHeader() {
+        return switch (fieldType) {
+            case MESSAGETYPE -> 0x00;
+            case ORIGINID -> 0x01;
+            case DESTID -> 0x02;
+            case COUNTER -> 0x03;
+            case BODY -> 0x04;
+        };
+    }
+
+    public abstract List<char> assemble();
+}
---
+++ src/node/membership/message/MessageTypeField.java
@@ -0,0 +1,31 @@
+package node.membership.message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageTypeField extends MessageField {
+    public MessageType messageType;
+
+    public MessageTypeField(FieldType fieldType, MessageType messageType) {
+        super(fieldType);
+        this.messageType = messageType;
+    }
+
+    private char translateType() {
+        return switch (messageType) {
+            case JOIN -> 0x00;
+            case LEAVE -> 0x01;
+            case MEMBERSHIP -> 0x02;
+        };
+    }
+
+    @Override
+    public List<char> assemble() {
+        List<char> field = new ArrayList<char>();
+
+        field.add(translateType());
+        field.add(translateFieldHeader());
+
+        return field;
+    }
+}
