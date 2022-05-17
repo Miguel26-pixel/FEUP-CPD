2ab7cfa63afb0ada9a144945dadc922cc5774969
David Preda <dtpreda30@gmail.com>
2022-05-17 19:05:00 +0100

    Start JoinMessage development

--- src/node/membership/message/JoinMessage.java
+++ src/node/membership/message/JoinMessage.java
@@ -1,7 +1,34 @@
 package node.membership.message;
 
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class JoinMessage extends Message {
-    public JoinMessage() { super(MessageType.JOIN); }
+    private final Integer counter;
+    private final Integer port;
+
+    public JoinMessage(int counter, int port) {
+        super(MessageType.JOIN);
+        this.counter = counter;
+        this.port = port;
+
+        this.buildBody();
+    }
+
+    private void buildBody() {
+        this.body = new ArrayList<byte>();
+        byte[] bytes = ByteBuffer.allocate(4).putInt(counter).array();
+
+        for (byte b : bytes) {
+            body.add(b);
+        }
+
+        body.add((byte) ' ');
+
+        bytes = ByteBuffer.allocate(4).putInt(port).array();
+
+        for (byte b : bytes) {
+            body.add(b);
+        }
+    }
 }
