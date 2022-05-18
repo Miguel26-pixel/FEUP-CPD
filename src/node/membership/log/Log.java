84444e0845cad5ba830504212dade6f37e0b087b
David Preda <dtpreda30@gmail.com>
2022-05-18 11:38:19 +0100

    Simplify MembershipMessage buildBody

--- src/node/membership/log/Log.java
+++ src/node/membership/log/Log.java
@@ -1,6 +1,9 @@
 package node.membership.log;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Log {
@@ -12,5 +15,37 @@ public class Log {
 
     public Map<String, LogEntry> getEntries() {
         return entries;
+    }
+
+    public List<Byte> toBytes() {
+        List<Byte> asBytes = new ArrayList<>();
+
+        for (Map.Entry<String, LogEntry> logEntry : entries.entrySet()) {
+            byte[] key = logEntry.getKey().getBytes();
+            for (byte b: key) {
+                asBytes.add(b);
+            }
+
+            asBytes.add((byte) ';');
+
+            byte[] counter = ByteBuffer.allocate(4).putInt(logEntry.getValue().getCounter()).array();
+
+            for (byte b : counter) {
+                asBytes.add(b);
+            }
+
+            asBytes.add((byte) ';');
+
+            byte[] epoch = ByteBuffer.allocate(4).putInt(logEntry.getValue().getEpoch()).array();
+
+            for (byte b : epoch) {
+                asBytes.add(b);
+            }
+
+            asBytes.add((byte) ' ');
+
+        }
+
+        return asBytes;
     }
 }
--- src/node/membership/message/MembershipMessage.java
+++ src/node/membership/message/MembershipMessage.java
@@ -18,31 +18,6 @@ public class MembershipMessage extends Message {
     }
 
     private void buildBody() {
-        this.body = new ArrayList<>();
-
-        for (Map.Entry<String, LogEntry> logEntry : log.getEntries().entrySet()) {
-            byte[] key = logEntry.getKey().getBytes();
-            for (byte b: key) {
-                this.body.add(b);
-            }
-
-            this.body.add((byte) ';');
-
-            byte[] counter = ByteBuffer.allocate(4).putInt(logEntry.getValue().getCounter()).array();
-
-            for (byte b : counter) {
-                this.body.add(b);
-            }
-
-            this.body.add((byte) ';');
-
-            byte[] epoch = ByteBuffer.allocate(4).putInt(logEntry.getValue().getEpoch()).array();
-
-            for (byte b : epoch) {
-                this.body.add(b);
-            }
-
-            this.body.add((byte) ' ');
-        }
+        this.body = log.toBytes();
     }
 }
