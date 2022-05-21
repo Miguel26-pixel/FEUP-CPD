d4f97481dc3d290555913f76202ebd098242c9d5
David Preda <dtpreda30@gmail.com>
2022-05-21 19:38:58 +0100

    Add MembershipMessage constructor from string

--- src/node/membership/message/MembershipMessage.java
+++ src/node/membership/message/MembershipMessage.java
@@ -2,6 +2,7 @@ package node.membership.message;
 
 import node.membership.log.Log;
 import node.membership.view.View;
+import node.membership.view.ViewEntry;
 
 public class MembershipMessage extends Message {
     private final static int RECENTENTRIESSIZE = 32;
@@ -11,6 +12,27 @@ public class MembershipMessage extends Message {
         super(MessageType.MEMBERSHIP);
         this.view = view;
         this.buildBody();
+    }
+
+    public MembershipMessage(String asString) {
+        super(MessageType.MEMBERSHIP);
+        this.view = extractString(asString);
+    }
+
+    private static View extractString(String asString) {
+        View view = new View();
+
+        String[] split = asString.split("\\|");
+        for(String entryString: split[0].split("\n")) {
+            String[] entry = entryString.split(";");
+            String id = entry[0];
+            String counter = entry[1];
+            String epoch = entry[2];
+
+            view.addEntry(entry[0], new ViewEntry(Integer.parseInt(counter), Integer.parseInt(epoch)));
+        }
+
+        return view;
     }
 
     private void buildBody() {
