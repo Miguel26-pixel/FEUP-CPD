6d5cf91f136244cad03481afb73306c5e83e80d1
David Preda <dtpreda30@gmail.com>
2022-05-25 12:26:11 +0100

    Finish JoinTask

--- src/node/membership/threading/JoinTask.java
+++ src/node/membership/threading/JoinTask.java
@@ -1,7 +1,11 @@
 package node.membership.threading;
 
 import message.messages.JoinMessage;
+import message.messages.MembershipMessage;
 import node.membership.view.View;
+
+import java.io.*;
+import java.net.Socket;
 
 public class JoinTask extends Thread {
     private final View view;
@@ -15,5 +19,24 @@ public class JoinTask extends Thread {
     @Override
     public void run() {
         JoinMessage joinMessage = new JoinMessage(joinMessageString);
+
+        if (!this.isCounterCorrect(joinMessage.getCounter())) {
+            return;
+        }
+
+        MembershipMessage reply = new MembershipMessage(this.view);
+
+        try {
+            Socket socket = new Socket(joinMessage.getIp(), joinMessage.getPort());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            out.write(reply.assemble());
+            socket.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+    private boolean isCounterCorrect(int counter) {
+        return counter % 2 == 0;
     }
 }
