0f2e663d55c8a4693033915b8539e6a4484d61cd
Fernando Rego <fernandoluisrego@gmail.com>
2022-05-25 19:14:35 +0100

    Fix message

--- src/Store.java
+++ src/Store.java
@@ -38,7 +38,7 @@ public class Store {
             Integer.parseInt(args[1]);
             Integer.parseInt(args[3]);
         } catch (NumberFormatException e) {
-            System.err.println("Invalid port");
+            System.err.println("Invalid port exception: " + e);
         }
 
         Node node = new Node(args[0], args[1], args[2], args[3]);
