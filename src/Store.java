8d3f1abac1357a68d9537201d3648da4f9442bc2
Fernando Rego <fernandoluisrego@gmail.com>
2022-05-25 19:03:38 +0100

    Add ip and port address validation

--- src/Store.java
+++ src/Store.java
@@ -1,5 +1,6 @@
 import client.Services;
 import node.Node;
+import utils.UtilsIP;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -11,6 +12,33 @@ public class Store {
         if (args.length != 4) {
             System.err.println("usage: java Store <IP_mcast_addr> <IP_mcast_port> <node_id> <Store_port>");
             System.exit(1);
+        }
+
+        if (UtilsIP.isIPValid(args[0])) {
+            System.err.println("Invalid multicast IP address");
+            System.exit(1);
+        }
+
+        if (UtilsIP.isPortValid(args[1])) {
+            System.err.println("Invalid multicast IP port");
+            System.exit(1);
+        }
+
+        if (UtilsIP.isIPValid(args[2])) {
+            System.err.println("Invalid node IP address");
+            System.exit(1);
+        }
+
+        if (UtilsIP.isPortValid(args[3])) {
+            System.err.println("Invalid multicast IP port");
+            System.exit(1);
+        }
+
+        try{
+            Integer.parseInt(args[1]);
+            Integer.parseInt(args[3]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid port");
         }
 
         Node node = new Node(args[0], args[1], args[2], args[3]);
