f17d825ae91df6aa0cd1c9eeac03926136e110be
David Preda <dtpreda30@gmail.com>
2022-05-28 11:38:38 +0100

    Make JoinTask update view

--- src/node/membership/threading/JoinTask.java
+++ src/node/membership/threading/JoinTask.java
@@ -3,6 +3,7 @@ package node.membership.threading;
 import message.messages.JoinMessage;
 import message.messages.MembershipMessage;
 import node.membership.view.View;
+import node.membership.view.ViewEntry;
 import utils.UtilsTCP;
 
 import java.io.*;
@@ -24,6 +25,9 @@ public class JoinTask extends Thread {
         if (!this.isCounterCorrect(joinMessage.getCounter())) {
             return;
         }
+
+        long secondsSinceEpoch = System.currentTimeMillis() / 1000;
+        this.view.addEntry(joinMessage.getOriginId(), new ViewEntry("Foda-se", "Isto não devia estar aqui", joinMessage.getCounter(), secondsSinceEpoch), true);
 
         MembershipMessage reply = new MembershipMessage(this.view);
 
