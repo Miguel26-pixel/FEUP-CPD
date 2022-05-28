01fd46055dc72e642a16ffe10e47a29ea76b421e
David Preda <dtpreda30@gmail.com>
2022-05-28 15:53:15 +0100

    Add abstract class CommunicationReceiver

---
+++ src/node/comms/CommunicationReceiver.java
@@ -0,0 +1,31 @@
+package node.comms;
+
+import node.membership.MembershipService;
+import node.store.KeyValueStore;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class CommunicationReceiver extends Thread {
+    protected final AtomicBoolean running;
+    protected final AtomicBoolean stop;
+
+    public CommunicationReceiver() {
+        this.running = new AtomicBoolean(true);
+        this.stop = new AtomicBoolean(false);
+    }
+
+    @Override
+    public void run() {
+        while (this.running.get()) {
+            try {
+                if (this.stop.get()) {
+                    Thread.sleep(1);
+                } else {
+                    this.read();
+                }
+            } catch (Exception ignore) {}
+        }
+    }
+
+    protected abstract void read();
+}
