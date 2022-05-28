4e26cd2701725beee26243b81f166c6953e1631a
David Preda <dtpreda30@gmail.com>
2022-05-28 11:08:35 +0100

    Add MembershipTask

--- src/node/membership/threading/MembershipTask.java
+++ src/node/membership/threading/MembershipTask.java
@@ -1,4 +1,21 @@
 package node.membership.threading;
 
+import message.messages.MembershipMessage;
+import node.membership.view.View;
+
 public class MembershipTask extends Thread {
+    private final View view;
+    private final String membershipMessageString;
+
+    public MembershipTask(View view, String membershipMessage) {
+        this.view = view;
+        this.membershipMessageString = membershipMessage;
+    }
+
+    @Override
+    public void run() {
+        MembershipMessage membershipMessage = new MembershipMessage(membershipMessageString);
+
+        this.view.copyView(membershipMessage.getView(), true);
+    }
 }
