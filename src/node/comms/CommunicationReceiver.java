beaa3bb5dee13205f31c4a994b1627c7045a5c61
David Preda <dtpreda30@gmail.com>
2022-05-28 16:18:40 +0100

    Add control methods to CommunicationReceiver

--- src/node/comms/CommunicationReceiver.java
+++ src/node/comms/CommunicationReceiver.java
@@ -1,24 +1,21 @@
 package node.comms;
 
-import node.membership.MembershipService;
-import node.store.KeyValueStore;
-
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class CommunicationReceiver extends Thread {
     protected final static int TIMEOUT = 3000;
 
-    protected final AtomicBoolean running;
+    protected final AtomicBoolean isAlive;
     protected final AtomicBoolean stop;
 
     public CommunicationReceiver() {
-        this.running = new AtomicBoolean(true);
+        this.isAlive = new AtomicBoolean(true);
         this.stop = new AtomicBoolean(false);
     }
 
     @Override
     public void run() {
-        while (this.running.get()) {
+        while (this.isAlive.get()) {
             try {
                 if (this.stop.get()) {
                     Thread.sleep(1);
@@ -27,6 +24,18 @@ public abstract class CommunicationReceiver extends Thread {
                 }
             } catch (Exception ignore) {}
         }
+    }
+
+    public void stopExecution() {
+        this.stop.set(true);
+    }
+
+    public void resumeExecution() {
+        this.stop.set(false);
+    }
+
+    public void killReceiver() {
+        this.isAlive.set(false);
     }
 
     protected abstract void read();
