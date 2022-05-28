fc9a7ca3105ef841ee680ca58fdf726b6c354d46
David Preda <dtpreda30@gmail.com>
2022-05-28 13:53:22 +0100

    Add first version of membership service

--- src/node/membership/MembershipService.java
+++ src/node/membership/MembershipService.java
@@ -1,20 +1,31 @@
 package node.membership;
 
-import message.Message;
-import message.messages.JoinMessage;
-import message.messages.LeaveMessage;
-import message.messages.MembershipMessage;
 import node.membership.threading.JoinTask;
 import node.membership.threading.LeaveTask;
 import node.membership.threading.MembershipTask;
 import node.membership.view.View;
 import threading.ThreadPool;
-import utils.UtilsTCP;
 
-import java.io.IOException;
-import java.net.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+public class MembershipService {
+    private final View view;
+    private final ThreadPool workers;
 
-public class MembershipService extends Thread {
+    public MembershipService() {
+        int numberOfCores = Runtime.getRuntime().availableProcessors();
+        this.workers = new ThreadPool(numberOfCores, numberOfCores);
 
+        this.view = new View();
+    }
+
+    public void processJoin(String joinMessageString) {
+        workers.execute(new JoinTask(this.view, joinMessageString));
+    }
+
+    public void processLeave(String leaveMessageString) {
+        workers.execute(new LeaveTask(this.view, leaveMessageString));
+    }
+
+    public void processMembership(String membershipMessageString) {
+        workers.execute(new MembershipTask(this.view, membershipMessageString));
+    }
 }
