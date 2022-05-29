fc01918d4606c879f8fa0a66fc94ed4c1d7254de
David Preda <dtpreda30@gmail.com>
2022-05-29 18:13:02 +0100

    Add compareSHA256

--- src/utils/UtilsHash.java
+++ src/utils/UtilsHash.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 
 public class UtilsHash {
     public static String hashSHA256(String input) {
@@ -21,5 +22,26 @@ public class UtilsHash {
         while (hexString.length() < 64) { hexString.insert(0, '0'); }
 
         return hexString.toString();
+    }
+
+    public static int compareSHA256(String first, String second) throws Exception {
+        if (first.length() < second.length()) {
+            throw new Exception("Both strings should be of equal size");
+        }
+
+        HexFormat hexFormat = HexFormat.of();
+
+        byte[] firstHex = hexFormat.parseHex(first);
+        byte[] secondHex = hexFormat.parseHex(second);
+
+        for (int i = 0; i < firstHex.length; i++) {
+            if (firstHex[i] > secondHex[i]) {
+                return 1;
+            } else if (firstHex[i] < secondHex[i]) {
+                return -1;
+            }
+        }
+
+        return 0;
     }
 }
