9483afa2cacc7fe8e39667d59c4a9b99b086e49a
David Preda <dtpreda30@gmail.com>
2022-05-30 10:28:38 +0100

    Fix error on LeaveMessage

--- src/message/messages/LeaveMessage.java
+++ src/message/messages/LeaveMessage.java
@@ -11,11 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LeaveMessage extends Message {
-    private final Integer counter;
+    private final String counter;
 
     public LeaveMessage(int counter, String originId) {
         super(MessageType.LEAVE, originId);
-        this.counter = counter;
+        this.counter = Integer.toString(counter);
 
         this.buildBody();
     }
@@ -23,16 +23,20 @@ public class LeaveMessage extends Message {
     public LeaveMessage(String asString) {
         super(MessageType.LEAVE);
 
-        List<String> split = new ArrayList<>(List.of(asString.split(CR.toString() + LF.toString())));
+        byte[] delim = new byte[]{CR,LF};
+
+        List<String> split = new ArrayList<>(List.of(asString.split(new String(delim))));
 
         split.removeIf(s -> s.equals(""));
+        split.remove(split.size() - 1);
 
         String body = split.get(split.size() - 1);
         split.remove(split.size() - 1);
 
-        for (int i = 0; i < split.size() / 2; i += 2) {
-            if (MessageField.translateFieldHeader(split.get(i)) == FieldType.ORIGINID) {
-                String originId = split.get(i + 1);
+        for (int i = 0; i < split.size(); i++) {
+            String[] field = split.get(i).split(" ");
+            if (MessageField.translateFieldHeader(field[0]) == FieldType.ORIGINID) {
+                String originId = field[1];
 
                 this.setOriginId(originId);
             }
@@ -40,19 +44,17 @@ public class LeaveMessage extends Message {
 
         List<String> params = new ArrayList<>(List.of(body.split(" ")));
 
-        this.counter = Integer.parseInt(params.get(0));
+        this.counter = params.get(0);
     }
 
     public Integer getCounter() {
-        return counter;
+        return Integer.parseInt(this.counter);
     }
 
     @Override
     protected void buildBody() {
         this.body = new ArrayList<>();
-        byte[] bytes = ByteBuffer.allocate(4).putInt(counter).array();
-
-        for (byte b : bytes) {
+        for (byte b : counter.getBytes()) {
             this.body.add(b);
         }
     }
